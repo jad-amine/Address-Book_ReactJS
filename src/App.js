7f369f0ce8042a2d706ce638e39395620c8501dd
jad amine <jade1992amine@gmail.com>
2022-06-24 13:17:52 +0300

    Linked Front and Back end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState("");
+
+  const getData = async () => {
+    const res = await axios("http://localhost:8000/");
+    return res.data;
+  };
+
+  useEffect(() => {
+    const getServerData = async () => {
+      const res = await getData();
+      setData(res.message);
+    };
+    getServerData();
+  }, []);
+
+  return <h2>{data}</h2>;
 }
 
 export default App;
