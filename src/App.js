e5597317080c804eb9926f6565547aef1513de73
jad amine <jade1992amine@gmail.com>
2022-06-24 20:01:29 +0300

    Added Navbar with style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Routes } from "react-router-dom";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [data, setData] = useState("");
 
   const getData = async () => {
-    const res = await axios("http://localhost:8000/");
-    return res.data;
+    const res = await axios("http://localhost:8000/contacts");
+    return res.data.contacts;
   };
 
   useEffect(() => {
     const getServerData = async () => {
       const res = await getData();
-      setData(res.message);
+      setData(res);
+      console.log(res);
     };
     getServerData();
   }, []);
 
-  return <h2>{data}</h2>;
+  return (
+    <div className="app">
+      <Navbar />
+      {/* <Routes></Routes> */}
+      <h1>Contacts</h1>
+      {/* {data &&
+        data.map((contact) => (
+          <div key={contact._id} className="contact">
+            <h2>{contact.name}</h2>
+            <h3>{contact.email}</h3>
+          </div>
+        ))} */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaUserPlus, FaListUl } from "react-icons/fa";
+
+const logo = require("../assets/download.png");
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" />
+      <ul>
+        <li>
+          <FaListUl />
+          <NavLink to="/Contacts">Contacts</NavLink>
+        </li>
+        <li>
+          <FaUserPlus />
+          <NavLink to="/addContacts">Add Contact</NavLink>
+        </li>
+      </ul>
+      <h1>Maps</h1>
+    </div>
+  );
+};
+
+export default Navbar;
