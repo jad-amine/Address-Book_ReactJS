abb9a5fc856677be39d7c4bb231bd84036ebf77a
jad amine <jade1992amine@gmail.com>
2022-06-26 08:50:59 +0300

    Added Place Contact Marker functionality

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,18 +1,43 @@
-import React, {useState} from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import '../App.css';
+import React, { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "../App.css";
 import { Icon } from "leaflet";
 
 function Map() {
+  const [location, setLocation] = useState("");
+
+  const LocationFinderDummy = () => {
+    const map = useMapEvents({
+      click(e) {
+        setLocation(e.latlng);
+      },
+    });
+    return null;
+  };
   return (
-    <MapContainer center={[34.249610, 35.665626]} zoom={12}scrollWheelZoom={true}>
+    <MapContainer
+      center={[34.24961, 35.665626]}
+      zoom={12}
+      scrollWheelZoom={true}
+    >
       <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <LocationFinderDummy />
+      {location && <Marker position={location}>
+        <Popup>
+          Contact Location.
+        </Popup>
+      </Marker>}
     </MapContainer>
   );
 }
 
 export default Map;
-
